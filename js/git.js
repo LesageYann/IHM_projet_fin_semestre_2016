5942b509f8884179f05ccab2f0b7289c70ea9065
radgeur <remy2112@hotmail.fr>
2016-04-22 18:53:54 +0200

    share works

--- js/git.js
+++ js/git.js
@@ -19,7 +19,11 @@ git.getHTML=function(){
     string += "<button id=\"init\"> Git init </button>  <span id=\"error_init\" class=\"error\"></span> <span id=\"right_init\" class=\"right\">  </span>    <br />";
     string += "<input type=\"text\" id=\"git commit\" placeholder=\"Nom du commit\">";
     string += "<button id=\"commit\" disabled=\"disabled\"> Commit </button>    <span id=\"error_commit\" class=\"error\"></span>   <span id=\"right_commit\" class=\"right\"></span>   <br />";
-    string += "<button id=\"share\"> Share </button><br />";
+    string += "Fichier partagé avec : <select id=\"usersShare\" size =\"1\"></select><br />";
+    string += "<input type=\"text\" id=\"user\" list=\"usersList\" placeholder=\"ex: user4\">";
+    string += "<datalist id=\"usersList\"></datalist>";
+    string += "<button id=\"addUsertoShare\"> Ajout </button>";
+    string += "<button id=\"delUsertoShare\"> Suppression </button><span id=\"error_add/del\" class=\"error\"></span>   <span id=\"right_add/del\" class=\"right\"></span><br />";
     string += "<button id=\"droits\"> Gérer les droits </button><br />";
 
     return string;
@@ -27,6 +31,7 @@ git.getHTML=function(){
 
 init.inject(git);
 
+//All the variables
 var initButton = document.getElementById("init");
 var initText = document.getElementById("git init");
 var commitButton = document.getElementById("commit");
@@ -37,10 +42,20 @@ var error_init = document.getElementById("error_init");
 var error_commit = document.getElementById("error_commit");
 var right_init = document.getElementById("right_init");
 var right_commit = document.getElementById("right_commit");
+var error_add_del = document.getElementById("error_add/del");
+var right_add_del = document.getElementById("right_add/del");
+var usersShare = document.getElementById("usersShare");
+var usersList = document.getElementById("usersList");
+var user = document.getElementById("user");
+var addUsertoShare = document.getElementById("addUsertoShare");
+var delUsertoShare = document.getElementById("delUsertoShare");
+var users = ["user1", "user2", "user3", "groupe1"];
+var allUsers = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "groupe1", "groupe2", "groupe3"];
 
-
+//hide the textbox for commit
 commitText.style.visibility = "hidden";
 
+//action when click on the button git init
 function submitGitInit(){
     var right = document.getElementById("true_init");
     if(initText.value == ""){
@@ -54,9 +69,9 @@ function submitGitInit(){
     initButton.disabled = "disabled";
     commitButton.disabled = "";
 }
-
 initButton.addEventListener("click", submitGitInit, false);
 
+//action when click on the button git commit
 function submitGitCommit(){
     right_init.innerHTML = "";
     if(commitText.value == ""){
@@ -70,5 +85,58 @@ function submitGitCommit(){
     initButton.disabled = "disabled";
     commitButton.disabled = "";
 }
-
 commitButton.addEventListener("click", submitGitCommit, false);
+
+//put the users into the select
+function usersIntoSelect(select) {
+    select.innerHTML = "";
+    for(var i = 0;i<users.length;i++)
+        select.innerHTML += "<option>" + users[i];
+}
+usersIntoSelect(usersShare);
+
+//put all the users into the dataList
+function addUsersToDataList(datalist){
+    datalist.innerHTML = "";
+    for(var i = 0;i<allUsers.length;i++)
+        datalist.innerHTML += "<option value=\"" + allUsers[i] + "\"/>";
+}
+addUsersToDataList(usersList);
+
+//add a User to the usersShare
+function addUser() {
+	if(allUsers.indexOf(user.value) != -1){
+		if(users.indexOf(user.value) == -1){
+			users.push(user.value);
+			usersIntoSelect(usersShare);
+			right_add_del.innerHTML = user.value+" a bien été ajouté.";
+            error_add_del.innerHTML = "";
+		}else{
+            right_add_del.innerHTML = "";
+			error_add_del.innerHTML = user.value+" a déjà accès à ce fichier."
+		}
+	}else{
+        right_add_del.innerHTML = "";
+		error_add_del.innerHTML = user.value+" n'existe pas!";
+	}
+}
+addUsertoShare.addEventListener("click", addUser, false);
+
+//del a User to the usersShare
+function delUser(){
+    if(allUsers.indexOf(user.value) != -1){
+        if(users.indexOf(user.value) == -1){
+            right_add_del.innerHTML = "";
+            error_add_del.innerHTML = user.value+" n'a pas accès à ce fichier.";
+        } else {
+            users.splice(users.indexOf(user.value),1);
+            usersIntoSelect(usersShare);
+            right_add_del.innerHTML = user.value+" a bien été supprimé.";
+            error_add_del.innerHTML = "";
+        }
+    }else {
+        right_add_del.innerHTML = "";
+        error_add_del.innerHTML = user.value + " n'existe pas";
+    }
+}
+delUsertoShare.addEventListener("click", delUser, false);
