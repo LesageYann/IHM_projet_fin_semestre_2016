b3a7ee87ff96d3ae913b337c267d90c3b8639871
lesage yann <yann.lesage@gmail.com>
2016-04-23 17:20:38 +0200

    init améliorer : configuration des click menu

--- js/init.js
+++ js/init.js
@@ -2,10 +2,31 @@
  * fichier qui se déclenche au onload.
  * s'occupe des injections de html dans la page
  */
-var init={};
+var init={moduleElem:{}};
 
 init.inject=function (module){
-    document.getElementById(module.id).innerHTML=module.getHTML();
+    init.moduleElem[module.id]=document.getElementById(module.id);
+    init.moduleElem[module.id].innerHTML=module.getHTML();
+    if(module.afterInject){
+        console.log("afterinject");
+        module.afterInject();
+    }
 };
 
+init.show=function(name){
+    for(var i in init.moduleElem){
+        init.moduleElem[i].style.display='none';
+    }
+    init.moduleElem[name].style.display='block';
+};
+
+document.getElementById("text_button").addEventListener('click',function(){
+    init.show('text');
+});
+document.getElementById("schema_button").addEventListener('click',function(){
+    init.show('schema');
+});
+document.getElementById("photo_button").addEventListener('click',function(){
+    init.show('photo');
+});
 
