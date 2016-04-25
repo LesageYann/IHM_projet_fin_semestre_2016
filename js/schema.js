4da64162a97e4ea96b60dce4b7025e39eb20c380
lesage yann <yann.lesage@gmail.com>
2016-04-26 00:42:45 +0200

    stroke/fill dispo, bouton aussi

--- js/schema.js
+++ js/schema.js
@@ -9,7 +9,13 @@ var schema ={
  * On peut utilisez les promises js si vous préférez
  */
 schema.getHTML=function(){
-    return "<div id=\"schema_flex\"><canvas id=\"schema_canvas\"></canvas><div id=\"schema_tools\"></div></div>";
+    return "<div id=\"schema_flex\"><canvas id=\"schema_canvas\"></canvas><div id=\"schema_tools\">"
+    +"<button id=\"schem_add\"><img src=\"css/add.png\" alt=\"new\" /></button>"
+    +"<button id=\"schem_path\"><img src=\"css/path.png\" alt=\"forme filaire\" /></button>"
+    +"<button id=\"schem_fill\"><img src=\"css/fill.png\" alt=\"forme remplie\" /></button>"
+    +"<button id=\"schem_attach\"><img src=\"css/attach.png\" alt=\"transferer vers Document\" /></button>"
+    +"<button id=\"schem_erase\"><img src=\"css/erase.png\" alt=\"effacer\" /></button>"
+    +"</div></div>";
 };
 
 schema.mouseDown=function(event){
@@ -26,7 +32,7 @@ schema.mouseMove=function(event){
 
 schema.mouseUp=function(event){
     console.log("mouseUp",event);
-    schema.ctx.fill();
+    schema.ctx[schema.pathMethod]();
 };
 
 schema.afterInject=function(){
@@ -37,6 +43,23 @@ schema.afterInject=function(){
     schema.canvas.onmousedown= schema.mouseDown;
     schema.canvas.onmousemove= schema.mouseMove;
     schema.canvas.onmouseup= schema.mouseUp;
+    schema.pathElem=document.getElementById("schem_path");
+    document.getElementById("schem_add").onclick=function(){
+        
+    }
+    schema.pathElem.onclick=function(){
+        schema.pathMethod= "stroke";
+//        schema.pathElem.parentNode.insertBefore(schema.strokeSize,schema.pathElem.nextSibling)
+    }
+    document.getElementById("schem_fill").onclick=function(){
+        schema.pathMethod= "fill";
+    }
+    document.getElementById("schem_attach").onclick=function(){
+        
+    }
+    document.getElementById("schem_erase").onclick=function(){
+        
+    }
 };
 
 init.inject(schema);
